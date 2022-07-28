fdf52a29311d680933014aa867703ba24456bfb7
Eunseo Choi <yses9296@gmail.com>
2022-07-28 20:43:24 +0900

    2022-07-27

--- vue_project/todo/src/App.vue
+++ vue_project/todo/src/App.vue
@@ -6,7 +6,7 @@
         <input type="text" class="form-control mb-4" v-model="userInput" @keyup.enter="addNewTodo">
 
         <div class="list-group">
-          <template v-for="todo in todoList.filter(todo => todo.state === 'active')" :key="todo">
+          <template v-for="todo in activeTodoList()" :key="todo">
             <button class="list-group-item text-left"  @click="toggleTodoState(todo)">
               {{ todo.label }}
             </button>
@@ -28,6 +28,9 @@
       };
     },
     methods: {
+      activeTodoList(){
+        return this.todoList.filter(todo => todo.state === 'active');
+      },
       addNewTodo(){
         this.todoList.push({
           label: this.userInput,
