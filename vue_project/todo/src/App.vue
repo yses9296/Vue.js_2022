5e689267a163f5fa142f7079dfc93c8d47bfb2fa
Eunseo Choi <yses9296@gmail.com>
2022-07-28 20:42:20 +0900

    2022-07-28

--- vue_project/todo/src/App.vue
+++ vue_project/todo/src/App.vue
@@ -3,7 +3,16 @@
     <div class="container">
       <div class="col-md-6 offset-md-3" >
         <h1 class="text-center mb-4">Todo Application</h1>
-        <input type="text" class="form-control" v-model="userInput">
+        <input type="text" class="form-control mb-4" v-model="userInput" @keyup.enter="addNewTodo">
+
+        <div class="list-group">
+          <template v-for="todo in todoList.filter(todo => todo.state === 'active')" :key="todo">
+            <button class="list-group-item text-left"  @click="toggleTodoState(todo)">
+              {{ todo.label }}
+            </button>
+          </template>
+
+        </div>
       </div>
     </div>
   </div>
@@ -14,8 +23,21 @@
     name: 'app',
     data() {
       return {
-        userInput: ''
+        userInput: '',
+        todoList: []
       };
+    },
+    methods: {
+      addNewTodo(){
+        this.todoList.push({
+          label: this.userInput,
+          state: 'active'
+        });
+        this.userInput = '';
+      },
+      toggleTodoState(todo){
+        todo.state = todo.state == 'active' ? 'done' : 'active';
+      }
     },
     component: {
 
