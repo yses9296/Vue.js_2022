3316cf05c472efea4bcad2578d55dcd74d6d1a80
Eunseo Choi <yses9296@gmail.com>
2022-07-28 20:30:57 +0900

    2022-07-28

---
+++ vue_project/todo/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="col-md-6 offset-md-3" >
+        <h1 class="text-center mb-4">Todo Application</h1>
+        <input type="text" class="form-control" v-model="userInput">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        userInput: ''
+      };
+    },
+    component: {
+
+    }
+  }
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
